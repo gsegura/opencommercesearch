fe38f9d84ac8640e1c86b5e018d4a8d937c13d3b
Gregorio Segura <gsegura@backcountry.com>
2014-12-16 14:46:03 -0600

    refactor plp revenue report to return the revenue for the top N pages

--- opencommercesearch-api/app/org/opencommercesearch/api/controllers/ReportsController.scala
+++ opencommercesearch-api/app/org/opencommercesearch/api/controllers/ReportsController.scala
@@ -18,29 +18,29 @@ object ReportsController extends  BaseController {
 
   val dateFormatter = new SimpleDateFormat("yyyy-MM-dd")
   val bcReportSuite = "bcbackcountry"
-  val overTimeReport = "Report.GetOvertimeReport"
+  val reportMethod = "Report.GetRankedReport"
 
-  @ApiOperation(value = "PLP Revenue", notes = "Returns the revenue for a plp page for a given time frame", httpMethod = "GET")
+  @ApiOperation(value = "Page Revenue", notes = "Returns the revenue for the top N pages for a given time frame", httpMethod = "GET")
   @ApiImplicitParams(value = Array(
-    new ApiImplicitParam(name = "pageType", value = "Page type & id", defaultValue = "PLP Cat:Men's Down Jackets", required = true, dataType = "string", paramType = "query")
+    new ApiImplicitParam(name = "top", value = "The top N results", defaultValue = "100", required = true, dataType = "string", paramType = "query")
   ))
-  def getPlpPageRevenue(version: Int,
+  def getPageRevenue(version: Int,
                         @ApiParam(value = "The report time window", required = true)
                         @PathParam("days")
                         days: Int) = Action.async { implicit request =>
 
-    val pageType = request.getQueryString("pageType").getOrElse("")
-    if (StringUtils.isEmpty(pageType)) {
-      Future.successful(BadRequest("Missing required pageType query parameter"))
+    val top = request.getQueryString("top").getOrElse("100")
+    if (StringUtils.isEmpty(top)) {
+      Future.successful(BadRequest("Missing required top query parameter"))
     } else {
       val dateFrom = dateFormatter.format(getLastXDays(-days))
       val dateTo = dateFormatter.format(new Date())
 
       //TODO gsegura: support adding the correct report suite per site, not hardcode bc report suite always
-      val jsonTemplate = "{\"reportDescription\": {\"reportSuiteID\": \"" + bcReportSuite + "\",\"dateFrom\": \"" + dateFrom + "\", \"dateTo\": \""+ dateTo + "\",\"metrics\": [{\"id\":\"revenue\"}],\"elements\":[{\"id\":\"eVar36\",\"selected\":[\"" + pageType + "\"]}]}}"
+      val jsonTemplate = "{\"reportDescription\": {\"reportSuiteID\": \"" + bcReportSuite + "\",\"dateFrom\": \"" + dateFrom + "\", \"dateTo\": \""+ dateTo + "\",\"metrics\": [{\"id\":\"revenue\"}],\"elements\":[{\"id\":\"page\",\"top\":" + top + "}]}}"
       val data = Json.parse(jsonTemplate)
 
-      OmnitureClient.callOmniture(data, overTimeReport).map { response =>
+      OmnitureClient.callOmniture(data, reportMethod).map { response =>
         Ok(response.json)
       }
     }
